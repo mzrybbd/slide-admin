5309d9a4758c0328d36f782117c0a73534fb5394
李娟 <lijuan@gaosiedu.com>
2020-04-23 18:49:44 +0800

    游戏上传

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,8 @@
  */
 import { extend } from 'umi-request';
 import { notification } from 'antd';
+const baseUrl = 'http://admin.slide.aixuexi.com'
+
 const codeMessage = {
   200: '服务器成功返回请求的数据。',
   201: '新建或修改数据成功。',
@@ -54,7 +56,9 @@ const errorHandler = error => {
 
 const request = extend({
   errorHandler,
+  prefix: baseUrl,
   // 默认错误处理
   credentials: 'include', // 默认请求是否带上cookie
 });
+
 export default request;
